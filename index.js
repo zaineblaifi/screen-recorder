e5915630c15f5af4c1e96418a3b192eaf6816648
zaineblaifi <112078103+zaineblaifi@users.noreply.github.com>
2023-06-14 19:25:14 +0200

    y

--- index.js
+++ index.js
@@ -1,11 +1,21 @@
-let mediaRecorder=null; // Déclaration de la variable mediaRecorder
-
+let mediaRecorder;
+const chunks = [];
+const recordings = [];
 function startRecording() {
-  navigator.mediaDevices.getDisplayMedia({ video: true, audio: true })
-    .then(function (stream) {
-      mediaRecorder = new MediaRecorder(stream); // Attribution à la variable globale
+  const displayOptions = {
+    video: { width: 1920, height: 1080, frameRate: 30 },
+    audio: {
+      echoCancellation: true, // Annulation de l'écho
+      noiseSuppression: true, // Suppression du bruit
+      sampleRate: 44100, // Taux d'échantillonnage audio (en Hz)
+      channelCount: 2, // Nombre de canaux audio (1 pour mono, 2 pour stéréo)
+    },
+  };
 
-      const chunks = [];
+  navigator.mediaDevices
+    .getDisplayMedia(displayOptions)
+    .then(function (stream) {
+      mediaRecorder = new MediaRecorder(stream);
 
       mediaRecorder.ondataavailable = function (e) {
         chunks.push(e.data);
@@ -13,26 +23,59 @@ function startRecording() {
 
       mediaRecorder.start();
 
-      mediaRecorder.onstop = function () {
-        const blob = new Blob(chunks, { type: 'video/webm' });
-
-        const downloadLink = document.createElement('a');
-        downloadLink.href = window.URL.createObjectURL(blob);
-        downloadLink.download = 'enregistrement.webm';
-
-        downloadLink.click();
-
-        window.URL.revokeObjectURL(downloadLink.href);
-      };
+      document.getElementById("start-button").disabled = true;
+      document.getElementById("stop-button").disabled = false;
     })
     .catch(function (error) {
-      console.error('Erreur lors de la capture de l\'écran et du son:', error);
+      console.error("Erreur lors de la capture de l'écran et du son:", error);
     });
 }
 
-const startButton = document.getElementById('start-button');
-if (startButton) {
-  startButton.addEventListener('click', startRecording);
+function stopRecording() {
+  mediaRecorder.stop();
+
+  mediaRecorder.onstop = function () {
+    const blob = new Blob(chunks, { type: "video/webm" });
+    chunks.length = 0;
+
+    const videoPreview = document.getElementById("preview");
+    videoPreview.src = URL.createObjectURL(blob);
+    videoPreview.controls = true;
+    /*const downloadLink = document.createElement('a');
+            downloadLink.href = URL.createObjectURL(blob);
+            downloadLink.download = 'enregistrement.webm';
+            downloadLink.click();*/
+    const recording = {
+      blob: blob,
+      url: URL.createObjectURL(blob),
+      name: `Recording ${recordings.length + 1}`,
+      date: new Date().toLocaleString(),
+    };
+
+    recordings.push(recording);
+    renderRecordings();
+    document.getElementById("start-button").disabled = false;
+    document.getElementById("stop-button").disabled = true;
+  };
+}
+function renderRecordings() {
+  const recordingsList = document.getElementById("recordings-list");
+  recordingsList.innerHTML = "";
+
+  recordings.forEach(function (recording, index) {
+    const recordingItem = document.createElement("li");
+    recordingItem.innerHTML = `
+            <span>${recording.name}</span>
+            <span>${recording.date}</span>
+            <a href="${recording.url}" download="${recording.name}.webm">Télécharger</a>
+            `;
+
+    recordingsList.appendChild(recordingItem);
+  });
 }
 
-  
+const startButton = document.getElementById("start-button");
+startButton.addEventListener("click", startRecording);
+
+const stopButton = document.getElementById("stop-button");
+stopButton.addEventListener("click", stopRecording);
